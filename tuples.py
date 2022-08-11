f2ea93a50ebcdf3a836b3e7f5775dd0103b765ce
siredwolfff <109526582+siredwolfff@users.noreply.github.com>
2022-08-11 18:30:20 +0530

    tuples+sets

---
+++ tuples.py
@@ -0,0 +1,39 @@
+my_tuple= (1,2,3,"Hello")
+print(my_tuple)
+
+my_tuple1 = ("Mouse", [1,2,3], [8,4,6])
+print(my_tuple1)
+
+my_tuple2= 3,4,6, "apple"
+print(my_tuple2)
+
+
+tuple1= ('p' , 'e', 'r' , 'm' , 'i' , 't')
+print(tuple1[0])
+print(tuple1[5])
+
+tuple2= ("Mouse", [8,4,6], [1,2,3])
+print(tuple2[0][3])
+print(tuple2[1][1])
+
+
+# Acessing tuple elements using slicing
+
+my_tuple1= ('e','x','c','a','l','i','b','e','r')
+print(my_tuple1[1:4])
+print(my_tuple1[:-7])
+print(my_tuple1[::7])
+print(my_tuple1[7:])
+print(my_tuple1[:])
+
+
+# concatenation
+output= (1,2,3,4,5,6)
+print((1,2,3) + (4,5,6))
+
+# repeat
+output= ('Repeat,' , 'Repeat,' , 'Repeat,')
+print(('Repeat,') * 3)
+
+my_tuple= ('A','P','P','L','E')
+print
